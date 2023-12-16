39de6f1c36cbcab6eb1faed8eb0efea4dedb5553
Hari-Ramesh <120625038+Hari-Ramesh@users.noreply.github.com>
2023-12-17 03:00:15 +0530

    Create Ansible.sh

---
+++ Ansible.sh
@@ -0,0 +1,6 @@
+# updating packages
+sudo yum update -y
+#Use the amazon-linux-extras command to install ansible.
+sudo amazon-linux-extras install ansible2 -y
+#Check Ansible Version
+ansible --version
