c77ced0445ad1b99ecf2bbac0ec82a412c5f6eb2
Hari-Ramesh <120625038+Hari-Ramesh@users.noreply.github.com>
2024-02-20 16:26:25 +0530

    Create terraform_installation.sh

---
+++ terraform_installation.sh
@@ -0,0 +1,5 @@
+wget https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_linux_amd64.zip
+unzip terraform_1.7.3_linux_amd64.zip
+mv terraform /usr/local/bin
+cd /usr/local/bin
+terraform -version
